6b6c0ea0e916d01031aea39c76b855feb8a6f458
pereudin <a.a.pereudin@gmail.com>
2019-10-19 08:16:01 +0300

    Initial commit

---
+++ app/src/main/java/ru/pereudin/hw4/gson/GsonActivity.java
@@ -0,0 +1,16 @@
+package ru.pereudin.hw4.gson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.pereudin.hw4.R;
+
+public class GsonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gson);
+    }
+}
